21f9dfc048f9be2f62be469588bbc238bcc1b9c7
kakuljain <kakuljain954@gmail.com>
2026-02-07 20:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (52.11%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        for (int i = 0; i + 1 < s.size(); i++) {
+            if (s[i] > s[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
