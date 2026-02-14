3e3a6288e40125d3a67d6606562358e4623f483b
kakuljain <kakuljain954@gmail.com>
2026-02-14 22:04:24 +0530

    Time: 32 ms (90.49%) | Memory: 111.2 MB (75.21%) - LeetSync

--- 4137-number-of-prefix-connected-groups/number-of-prefix-connected-groups.cpp
+++ 4137-number-of-prefix-connected-groups/number-of-prefix-connected-groups.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int prefixConnected(vector<string>& words, int k) {
-        
-        int n = words.size();
-        if(n < 2) return 0;
-        
-        sort(words.begin(), words.end());
-        
-        int ans = 0;
-        int cnt = 0;   
-        
-        for(int i = 1; i < n; i++) {
-            
-            if(words[i].size() >= k && words[i-1].size() >= k &&
-               words[i].substr(0,k) == words[i-1].substr(0,k)) {
-                
-                cnt++;  
-            }
-            else {
-                if(cnt > 0) ans++;   
-                cnt = 0;            
+        unordered_map<string,int>um;
+
+        for(auto &w:words){
+            if(w.size()>=k){
+                string s=w.substr(0,k);
+                um[s]++;
             }
         }
-        
-        if(cnt > 0) ans++;  
-        
-        return ans;
+        int count=0;
+        for(auto &it:um){
+            if(it.second >=2)
+                 count++;
+        }
+        return count;
     }
 };
