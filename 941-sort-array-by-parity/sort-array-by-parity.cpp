e617ab290e0492ac7987151095f5ebbe00eec369
kakuljain <kakuljain954@gmail.com>
2026-03-28 19:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (43.57%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         vector<int> ans;
 
-        // even numbers
+        
         for(int i = 0; i < nums.size(); i++)
         {
             if(nums[i] % 2 == 0)
