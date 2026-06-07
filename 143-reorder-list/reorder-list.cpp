352a296d30855ef0729b661288f0967152803b96
kakuljain <kakuljain954@gmail.com>
2026-06-07 14:05:04 +0530

    Time: 3 ms (12.83%) | Memory: 22.8 MB (76.92%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -10,39 +10,40 @@
  */
 class Solution {
 public:
-    ListNode *reverse(ListNode* h1) {
-        ListNode* prev = nullptr;
-        ListNode* curr = h1;
-        while (curr) {
-            ListNode* nextnode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextnode;
-        }
-        return prev;
+    ListNode*reverse(ListNode*h1)
+    {
+        ListNode*prev=nullptr;
+        ListNode*curr=h1;
+        while(curr)
+        {
+        ListNode*nextnode=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nextnode;
+        }return prev;
     }
     void reorderList(ListNode* head) {
         if(!head||!head->next) return;
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        ListNode* t1 = reverse(slow->next);
-        slow->next = nullptr;
-        ListNode* first = head;
-        ListNode* second = t1;
-        while(second)
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast&&fast->next)
         {
-            ListNode*f1=first->next;
-            ListNode*s1=second->next;
-            first->next=second;
-            second->next=f1;
-            first=f1;
-            second=s1;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-       
+        ListNode*t1=reverse(slow->next);
+        slow->next=nullptr;
+        ListNode*f=head;
+        ListNode*s=t1;
+        while(s)
+        {
+            ListNode*f1=f->next;
+            ListNode*s1=s->next;
+            f->next=s;
+            s->next=f1;
+            f=f1;
+            s=s1;
+        }
+        
     }
 };
