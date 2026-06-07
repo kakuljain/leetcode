0e620be2f20e41e56192b86d9eebaf13f0c849db
kakuljain <kakuljain954@gmail.com>
2026-06-07 15:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (57.14%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-
-        string s = "";
-
-        while (n > 0) {
-            s += (n % 2) + '0';
-            n /= 2;
+        int s=1;
+        while(s<=n)
+        {
+            s=s*2;
         }
-
-        reverse(s.begin(), s.end());
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '0') {
-                s[i] = '1';
-            }
-        }
-        int ans = 0;
-
-        for (char ch : s) {
-            ans = ans * 2 + (ch - '0');
-        }
-        return ans;
+        return s-1;
     }
 };
